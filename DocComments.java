b0083cc5014749edd5197bfbe9daa7df5644ab76
may-leth <mayleth9609@gmail.com>
2025-05-08 14:21:50 +0200

    Add javaDoc and function bmiResults

--- DocComments.java
+++ DocComments.java
@@ -6,11 +6,14 @@ public class DocComments {
     
     public static void main(String[] args) {
         greeting();
-        greetingCoder("Alex", 155, 44.50);
+        greetingCoder("Alex", 1.55, 44.50);
         double bmi = calculateBodyMassIndex(1.55, 44.50);
         System.out.println(bmi);
 
-        //Ejecuta el nuevo método 
+        //Ejecuta el nuevo método
+        bmiResults(bmi);
+        double otherBmi = calculateBodyMassIndex(1.60, 80);
+        bmiResults(otherBmi);
     }
 
     /**
@@ -56,7 +59,6 @@ public class DocComments {
     }
 
     //Escribe una función que con el índice de masa corporal devuelva un String con los resultados y documéntala:
-    
     /* Clasificación índice de masa corportal rango - kg/m2
     Delgadez severa	< 16
     Delgadez moderada	16 - 17
@@ -67,6 +69,30 @@ public class DocComments {
     Obeso Clase II	35 - 40
     Obeso Clase III	> 40 */
 
+    public static void bmiResults(double bmi) {
+        if (bmi < 16) {
+            System.out.println("Delgadez Severa");
+        } else if (bmi >= 16 && bmi < 17) {
+            System.out.println("Delgadez moderada");
+        } else if (bmi >= 17 && bmi < 18.5) {
+            System.out.println("Delgadez leve");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normal");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.println("Sobrepeso");
+        } else if (bmi >= 30 && bmi < 35) {
+            System.out.println("Obeso Clase I");
+        } else if (bmi >= 35 && bmi < 40) {
+            System.out.println("Obeso Clase II");
+        } else {
+            System.out.println("Obeso Clase III");
+        }
+    }
 
-
+    /**
+     * Function name: bmiResults
+     * @param bmi (double)
+     * Inside the function:
+     * 1. Prints a message depending on the BMI classification range.
+     */
 }
